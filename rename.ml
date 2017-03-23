d32d7bdc9530af6bb4e32b7d73a0b942b19b3348
Suwa (K1Laptop03 Windows) <5546507742@mail.ecc.u-tokyo.ac.jp>
2017-03-23 12:49:06 +0900

    change rename.ml

--- rename.ml
+++ rename.ml
@@ -1,13 +1,14 @@
 open Types
 
 
-type id = int
-type env = (variable_name * int) list
+type id = int * string
+type env = (variable_name * id) list
 
 
 let current_rename_id = ref 0
 
-let string_of_id evid = "V" ^ (string_of_int evid)
+let string_of_id (n, s) =
+  match s with "" -> "V" ^ (string_of_int n) | _ -> s
 
 let initialize () =
   current_rename_id := 0
@@ -15,19 +16,19 @@ let initialize () =
 let empty = []
 
 let fresh () =
-  let evid = !current_rename_id in
-    current_rename_id := evid + 1 ;
+  let evid = (!current_rename_id, "") in
+    current_rename_id := !current_rename_id + 1 ;
     evid
 
 let add (rnenv : env) (varnm : variable_name) =
-  let evid = !current_rename_id in
+  let evid = (!current_rename_id, varnm) in
   let rec aux acc lst =
     match lst with
     | []                             -> (varnm, evid) :: rnenv
     | (v, e) :: tail  when v = varnm -> List.rev_append acc ((v, e) :: tail)
     | (v, e) :: tail                 -> aux ((v, e) :: acc) tail
   in
-    current_rename_id := evid + 1 ;
+    current_rename_id := !current_rename_id + 1 ;
     (aux [] rnenv, evid)
 
 let rec find (rnenv : env) (varnm : variable_name) =
